2a53af66f3ab7c5710b8d03478d1f2b050a876ec
kortby@gmail.com <kortby@gmail.com>
2026-09-21 17:23:53 -0700

    fix: change default appName fallback from Laravel to empty string

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -21,7 +21,7 @@ if (broadcaster === 'pusher') {
     });
 }
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || '';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
